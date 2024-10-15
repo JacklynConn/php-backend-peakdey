f4bd2ea475b748645b024629bcf41f76818249ed
makmach <471842@ntti.edu.kh>
2024-10-15 22:02:53 +0700

    #41 get data for city item

---
+++ home-city41/file/menu.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 menu">
+        <ul>
+            <li>
+                <a href="index.php?lang=<?php echo $lang; ?>">
+                    Home
+                </a>
+            </li>
+            <?php
+            $sql = "SELECT id, city_name FROM tbl_city WHERE status=1 && lang=$lang ORDER BY od";
+            $rs = $cn->query($sql);
+            while ($row = $rs->fetch_assoc()) {
+            ?>
+                <li>
+                    <a href="index.php?lang=<?php echo $lang; ?>&city_id=<?php echo $row['id']; ?>">
+                        <?php echo $row['city_name']; ?>
+                    </a>
+                </li>
+            <?php
+            }
+            ?>
+            <li>
+                <a href="index.php?lang=1">English</a>
+            </li>
+            <li>
+                <a href="index.php?lang=2">Khmer</a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ home-city41/index.php
@@ -0,0 +1,51 @@
+<?php
+// open connection
+$cn = new mysqli('localhost', 'root', '', 'php_backend_db');
+if ($cn->connect_error) {
+    die('Connection failed: ' . $cn->connect_error);
+}
+// UTF-8
+$cn->set_charset('utf8');
+
+$lang = 1;
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample1</title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- Menu -->
+        <?php
+            include('file/menu.php');
+        ?>
+        
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 title1-box">
+                <h1>Khmer Empire</h1>
+                <p>Description</p>
+            </div>
+        </div>
+        <!-- Item -->
+        <?php
+            if(isset($_GET['city_id'])){
+                include('file/city-item.php');
+            }else{
+                include('file/item.php');
+            }
+            
+        ?>
+    </div>
+</body>
+
+</html>
