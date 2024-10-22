a9555de905af618445c88960e6cd115433655882
makmach <471842@ntti.edu.kh>
2024-10-22 23:57:53 +0700

    #45 create admin styles and layout

---
+++ project45/admin/admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="bar1">
+        <div class="box1">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="box2">
+            Rean Web
+        </div>
+        <div class="box3">
+            <span>
+                makmach1122@gmail.com
+            </span>
+            <i class="fa-solid fa-right-from-bracket"></i>
+        </div>
+    </div>
+</body>
+
+</html>
