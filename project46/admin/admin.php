bd602740a208b809dc17980fab8b35ab961d0111
makmach <471842@ntti.edu.kh>
2024-10-23 11:26:25 +0700

    #46 Update admin styles and layout (add, search, select)

---
+++ project46/admin/admin.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="bar1">
+        <div class="box1">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="box2">
+            Rean Web
+        </div>
+        <div class="box3">
+            <span>
+                makmach1122@gmail.com
+            </span>
+            <i class="fa-solid fa-right-from-bracket"></i>
+        </div>
+    </div>
+    <div class="left-menu">
+        <ul>
+            <li>
+                <a>
+                    <i class="fa-solid fa-user-gear"></i>
+                    <span>
+                        System
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="">User</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a>
+                    <i class="fa-solid fa-plus"></i>
+                    <span>
+                        Setup
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="">Category</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="bar2">
+            <div class="box1">
+                <div class="btnAdd">
+                    <i class="fa-solid fa-plus"></i>
+                    Add
+                </div>
+                <div class="search-box">
+                    <input type="text" placeholder="Search">
+                    <select name="" id="">
+                        <option value="">Category</option>
+                        <option value="">News</option>
+                    </select>
+                    <div class="btnSearch">
+                        <i class="fa-solid fa-search"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="box2">
+                2
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
